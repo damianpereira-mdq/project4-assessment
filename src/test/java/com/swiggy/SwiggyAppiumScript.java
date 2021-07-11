e2e43f49157b19ff8eb2e00fb456a866f4ee62b2
damianpereira-mdq <damianpereira.mdq@gmail.com>
2021-07-11 01:34:18 -0300

    Test AWS 1

--- src/test/java/com/swiggy/SwiggyAppiumScript.java
+++ src/test/java/com/swiggy/SwiggyAppiumScript.java
@@ -4,45 +4,47 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
-import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
 
 public class SwiggyAppiumScript {
 	
-	static AndroidDriver<MobileElement> driver;	
+	static AndroidDriver<AndroidElement> driver = null;
 	
 	@Test
 	public void StartConnection () throws MalformedURLException {
 
 		
-		DesiredCapabilities cap = new DesiredCapabilities();		
-		//cap.setCapability("platformName", "Android");
-		//cap.setCapability("platformVersion", "7.1.1");
-		//cap.setCapability("remoteAdbHost", "127.0.0.1");
-		//cap.setCapability("adbPort", "6080");
 		
+		DesiredCapabilities cap = new DesiredCapabilities();			
 		
-		//cap.setCapability("app", "/swiggy-3-69-1.apk");
-		//cap.setCapability("appPackage", "in.swiggy.android");
-		//cap.setCapability("appActivity", "in.swiggy.android.activities.HomeActivity");
-		//cap.setCapability(AndroidMobileCapabilityType.AUTO_GRANT_PERMISSIONS, true);
+		cap.setCapability("deviceName", "nexus_6_5.1.1");		
+		cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+		cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11");
+		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UIAutomator2");
+		cap.setCapability("systemPort", 8201);
 		
+		//cap.setCapability("appPackage", "com.miui.calculator");
+		//cap.setCapability("appActivity", "com.miui.calculator.cal.CalculatorActivity");		
+			
 		
 		//This is Docker 		
-		URL url = new URL("http://18.221.185.200:4444/wd/hub/");
+		URL url = new URL("http://127.0.0.1:4444/wd/hub");
 
 		//This is local Appium
-		//URL url = new URL("http://127.0.0.1:4723/wd/hub/");
+		//URL url = new URL("http://127.0.0.1:4723/wd/hub/");		
 		
-		
-		driver = new AndroidDriver<MobileElement>(url, cap);		
+		driver = new AndroidDriver<>(url, cap);		
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		System.out.println("Connection successfuly started!");
+		driver.quit();
 	}
 
 	

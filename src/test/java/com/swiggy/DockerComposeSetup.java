7edbd588eb4d39755eaa1f09fe7863c3778dc491
damianpereira-mdq <damianpereira.mdq@gmail.com>
2021-07-11 20:39:42 -0300

    Purged

--- src/test/java/com/swiggy/DockerComposeSetup.java
+++ src/test/java/com/swiggy/DockerComposeSetup.java
@@ -1,6 +1,8 @@
 package com.swiggy;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -36,8 +38,21 @@ public class DockerComposeSetup {
 
 		System.out.println("Starting Docker-compose");
 
-		DockerComposeSetup dockerComposeSetup = new DockerComposeSetup();
-		dockerComposeSetup.runScript("sh start_dockerCompose.sh");	
+		try {
+            Process proc = Runtime.getRuntime().exec("sh start_dockerCompose.sh"); //Whatever you want to execute
+            BufferedReader read = new BufferedReader(new InputStreamReader(
+                    proc.getInputStream()));
+            try {
+                proc.waitFor();
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+            while (read.ready()) {
+                System.out.println(read.readLine());
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
 
 		Thread.sleep(25000);		
 
@@ -46,8 +61,21 @@ public class DockerComposeSetup {
 		@AfterTest
 		public void stopDocker () throws IOException, InterruptedException {
 
-			DockerComposeSetup dockerComposeSetup = new DockerComposeSetup();
-			dockerComposeSetup.runScript("sh stop_dockerCompose.sh");	
+			try {
+	            Process proc = Runtime.getRuntime().exec("sh stop_dockerCompose.sh"); //Whatever you want to execute
+	            BufferedReader read = new BufferedReader(new InputStreamReader(
+	                    proc.getInputStream()));
+	            try {
+	                proc.waitFor();
+	            } catch (InterruptedException e) {
+	                System.out.println(e.getMessage());
+	            }
+	            while (read.ready()) {
+	                System.out.println(read.readLine());
+	            }
+	        } catch (IOException e) {
+	            System.out.println(e.getMessage());
+	        }	
 
 			Thread.sleep(25000);						
 

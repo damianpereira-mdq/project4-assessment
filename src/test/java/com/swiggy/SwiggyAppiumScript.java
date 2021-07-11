20ca9d563a63f5370ca7e36682a6691a77e21caa
damianpereira-mdq <damianpereira.mdq@gmail.com>
2021-07-11 14:15:03 -0300

    13849b

--- src/test/java/com/swiggy/SwiggyAppiumScript.java
+++ src/test/java/com/swiggy/SwiggyAppiumScript.java
@@ -57,7 +57,7 @@ public class SwiggyAppiumScript {
 			
 		
 		//This is Docker 		
-		URL url = new URL("http://0.0.0.0:49155/wd/hub");
+		URL url = new URL("http://0.0.0.0:49153/wd/hub");
 
 		//This is local Appium
 		//URL url = new URL("http://127.0.0.1:4723/wd/hub/");		
